077b4bee3f2ce61aded5622d34029b67d28b3d8c
mdpasha7 <mdpasha7970@gmail.com>
2023-08-18 07:41:47 +0530

    jsx components

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 # npm install
 # npx parcel index.html
 
+# to start the project do run below commands
+ - npm install
+ - npm run start 
+ - npm run build
+
 # parcel 
 - Dev Build 
 - Local Server
--- app.js
+++ app.js
@@ -8,10 +8,49 @@ import ReactDOM from 'react-dom';
         </div>
    </div>
  */
-
-const nestedElements = React.createElement('div',{id:'parent'},
-React.createElement('div',{id:'child'}),
-React.createElement('h1',{id:'text'},'Hello all, i am form reactjs and nested h1'));
+// React create Element
+const nestedElements = React.createElement('div',{id:'parent'},'Hello all, i am form reactjs and nested h1');
+//Reactjs element is an object
 console.log(nestedElements);
 const root2 = ReactDOM.createRoot(document.getElementById('root2'));
-root2.render(nestedElements);
+root2.render(nestedElements);// reactjs element - html element 
+
+//jsx 
+//babel-
+let name = "My name is Mohammad Pasha";
+let number = "8501012491";
+const jsxdivision = <div id='parent' className='parent-class'>Hello,this is jsx and {name} and mobile number is {number}
+<h1 id='heading'>Heading element</h1>
+</div>;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(jsxdivision);
+
+// React Components
+// Everything in React Components - 2 ways 
+// 1) Class Based Components (OLD way)
+// 2) Functional Based Components (99.999 we use this)
+const ButtonCom = () => (
+  <input type='text'/>
+);
+const HeaderComponent = () => (
+  <div class="header"><h1>My Header</h1><ButtonCom/></div>
+);
+const ContentComponent = () => (
+  <div class="content"><h1>My Content</h1></div>
+);
+const FooterComponent = () => (
+  <div class="footer"><h1>My Footer</h1></div>
+);
+
+const HomePageComponent = () => (
+  <div className='container'>
+    <HeaderComponent></HeaderComponent>
+    <ContentComponent></ContentComponent>
+    <FooterComponent></FooterComponent>
+  
+  </div>
+)
+
+
+const root3 = ReactDOM.createRoot(document.getElementById('root3'));
+root.render(<HomePageComponent/>);
