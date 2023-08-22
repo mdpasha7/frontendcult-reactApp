4151979ed09a73a3fb74c91516b2658cc427785e
mdpasha7 <mdpasha7970@gmail.com>
2023-08-22 22:41:14 +0530

    appending data

--- app1.js
+++ app1.js
@@ -37,15 +37,101 @@ const HeaderComponent = () => {
         </div>
     )
 };
+
+const restaurantData = {
+      id: "57903",
+      name: "Capital Multi Cuisine Restaurant",
+      cloudinaryImageId: "ijy2jxi7lfwsebdtazpi",
+      areaName: "Malakpet",
+      costForTwo: "₹500 for two",
+      cuisines: [
+        "Biryani",
+        "Arabian",
+        "Lebanese"
+      ],
+      avgRating: 3.7,
+      feeDetails: {
+        restaurantId: "57903",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 3100
+          },
+          {
+            name: "BASE_TIME"
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE"
+          }
+        ],
+        totalFee: 3100
+      },
+      parentId: "19262",
+      avgRatingString: "3.7",
+      totalRatingsString: "10K+",
+      sla: {
+        deliveryTime: 19,
+        lastMileTravel: 3.7,
+        serviceability: "SERVICEABLE",
+        slaString: "19 mins",
+        lastMileTravelString: "3.7 km",
+        iconType: "ICON_TYPE_EMPTY"
+      },
+      availability: {
+        nextCloseTime: "2023-08-23 02:00:00",
+        opened: true
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {}
+        }
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        subHeader: "UPTO ₹100",
+        discountCalloutInfo: {
+          message: "Free Delivery",
+          logoCtx: {
+            logo: "v1655895371/free_delivery_logo_hqipbo.png"
+          }
+        }
+      },
+      orderabilityCommunication: {
+        title: {},
+        subTitle: {},
+        message: {},
+        customIcon: {}
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {}
+        }
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {}
+    };
+
 const RestoComponent = (props) => {
     console.log(props);
+    const {restoData} = props;
+    console.log(restoData);
     return (
         <div className="resto">
             <img src="https://tse2.mm.bing.net/th?id=OIP.Yry1dvxtNwfagdBt9MQT4wHaG-&pid=Api&P=0&h=180" alt="tifin logo"/>
-            <h2>{props.resName}</h2>
-            <p>{props.resMake}</p>
-            <h4>{props.resRating}</h4>
-            <h4>{props.resDeliverytime}</h4>
+            <h2>{restoData.name}</h2>
+            <h4>{restoData.avgRating}</h4>
+            <h4>{restoData.areaName}</h4>
+            <h4>{restoData.costForTwo}</h4>
+            <h4>{restoData.cuisines.join(',')}</h4>
 
         </div>
     )
@@ -56,13 +142,9 @@ const BodyComponent = () => {
         
         <div className="body">
             <div className="resto-container">
-            <RestoComponent 
-             resName= 'Varalakshmi'
-             resRating = '3.6'
-             resDeliverytime = '30min'
-             resMake = 'Biryani, Tandoori'
-            />
-            <RestoComponent
+            <RestoComponent restoData = {restaurantData}/>
+            <RestoComponent restoData = {restaurantData}/>
+            {/* <RestoComponent
              resName= 'KFC Food'
              resRating = '3.8'
              resDeliverytime = '40min'
@@ -79,7 +161,7 @@ const BodyComponent = () => {
              resRating = '4.0'
              resDeliverytime = '20min'
              resMake = 'Biryani'
-            />
+            /> */}
             </div>
         </div>
     )
